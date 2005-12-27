0b0b752f02e9c6b28a99c3de6caf9f226f176eac
kenny <>
2005-12-27 14:52:36 +0000

    initial.

---
+++ src/org/pgist/wfengine/tasks/InputNameTask.java
@@ -0,0 +1,29 @@
+package org.pgist.wfengine.tasks;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.pgist.wfengine.ManualTask;
+import org.pgist.wfengine.Workflow;
+
+
+/**
+ * 
+ * @author kenny
+ *
+ * @hibernate.joined-subclass name="InputNameTask" table="litwf_inputnametask"
+ * @hibernate.joined-subclass-key column="id"
+ */
+public class InputNameTask extends ManualTask {
+    
+    
+    public InputNameTask() {
+    }
+    
+    
+    protected String execute(Workflow workflow, HttpServletRequest request, HttpServletResponse response) {
+        return "";
+    }//execute
+    
+    
+}//class InputNameTask
