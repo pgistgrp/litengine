aa90c5e233379d444e1b802cb996479bf2f112c8
kenny <>
2006-02-10 14:39:29 +0000

    initial.

---
+++ src/org/pgist/wfengine/WorkflowEngineDAO.java
@@ -0,0 +1,39 @@
+package org.pgist.wfengine;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.pgist.wfengine.activity.GroupActivity;
+import org.pgist.wfengine.activity.PActActivity;
+
+
+
+/**
+ * The interface WorkflowEngineDAO.
+ * 
+ * @author kenny
+ *
+ */
+public interface WorkflowEngineDAO {
+    
+    
+    Workflow getWorkflow(Long id, boolean finished, boolean cancelled) throws Exception;
+    
+    Session getHibernateSession() throws Exception;
+    
+    void saveTemplate(Template template) throws Exception;
+    
+    void saveWorkflow(Workflow workflow) throws Exception;
+    
+    Template getTemplate(Long id) throws Exception;
+    
+    void saveActivity(Activity activity) throws Exception;
+    
+    PActActivity getPActActivityByRefId(Long refId) throws Exception;
+    
+    GroupActivity getGroupActivityByRefId(Long level, Long refId) throws Exception;
+
+    List getTemplates(int type) throws Exception;
+    
+    
+}//interface WorkflowEngineDAO
