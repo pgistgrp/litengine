cd4f0ae6822b6eb061de4b3e6cfc19f04e42e474
kenny <>
2006-08-14 05:17:40 +0000

    *** empty log message ***

--- src/org/pgist/wfengine/Environment.java
+++ src/org/pgist/wfengine/Environment.java
@@ -1,15 +1,21 @@
 package org.pgist.wfengine;
 
+import java.util.HashMap;
+import java.util.Map;
+
 
 /**
  * 
  * @author kenny
  *
+ * @hibernate.class table="litwf_env"
  */
 public class Environment {
-
+    
     
     private Long id;
+    
+    private Map map = new HashMap();
     
 
     /**
@@ -26,13 +32,39 @@ public class Environment {
     }
     
     
+    /**
+     * @return
+     * 
+     * @hibernate.map table="litwf_env_var_link"
+     * @hibernate.collection-key column="env_id"
+     * @hibernate.collection-index column="env_var_name" type="string"
+     * @hibernate.collection-many-to-many class="org.pgist.wfengine.EnvironmentVariable" column="env_var_id"
+     */
+    public Map getMap() {
+        return map;
+    }
+
+
+    public void setMap(Map map) {
+        this.map = map;
+    }
+    
+    
+    /*
+     * ------------------------------------------------------------------------
+     */
+    
+    
     public Object get(String name) {
-        return null;
+        return map.get(name);
     }
     
     
     public void put(String name, Object value) {
+        EnvironmentVariable var = new EnvironmentVariable();
+        var.setObject(value);
+        map.put(name, var);
     }
-    
-    
+
+
 }//class Environment
